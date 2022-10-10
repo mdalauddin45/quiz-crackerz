faf56ffe72e240cb1cc5188b6992885c48cbb396
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-10 22:58:40 +0600

    hero icon i

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,70 +4,28 @@ const Question = ({ quiz }) => {
   console.log(quiz);
   const { question, correctAnswer, id, options } = quiz;
   return (
-    <div>
-      <div
-        v-if="questionIndex === 0"
-        class="slide slide0"
-        key="slide0"
-        v-quiz-set='{question: {"id":177,"key":0,"multiple":false}, answers: [{"id":627,"answer":"Database"},{"id":628,"answer":"Connectivity"},{"id":629,"answer":"User interface"},{"id":630,"answer":"Design Platform"}] }'
-      >
-        <div class="flex items-center mb-4">
-          <span class="inline-block rounded-full py-1 px-3 text-white bg-blue-200 mr-3 shadow-lg">
-            1/20
-          </span>
-          <p> React is mainly used for building ___.</p>
-        </div>
-
-        <div class="answer-container">
-          <div class="row">
-            <div class="answer col-sm-12">
-              <ul>
-                <li
-                  v-for="answer in answers[0]"
-                  click="toggle($event)"
-                  class="rounded-lg bg-gray-100 hover:bg-gray-200 mb-4 p-4 cursor-pointer"
-                >
-                  <label class="flex items-center">
-                    <input
-                      name="answer"
-                      value="answer.id"
-                      v-model="questions[0].selected"
-                      type="questions[0].multiple ? 'checkbox' : 'radio' "
-                    />
-                    <span
-                      v-text="answer.answer"
-                      class="inline-block ml-2 text-gray-600"
-                    ></span>
-                  </label>
-                </li>
-              </ul>
-            </div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+      <section className=" dark:text-gray-900 bg-gray-100 p-6 rounded shadow-lg">
+        <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+          <h2 className="text-2xl font-semibold sm:text-4xl mt-4 mb-10">
+            {question}
+          </h2>
+          <div className="space-y-4">
+            <p className="w-96 border rounded-lg px-4 py-4 focus:outline-none focus-visible:ring-violet-400">
+              {options[0]}
+            </p>
+            <p className="w-96 border rounded-lg px-4 py-4 focus:outline-none focus-visible:ring-violet-400">
+              {options[1]}
+            </p>
+            <p className="w-96 border rounded-lg px-4 py-4 focus:outline-none focus-visible:ring-violet-400">
+              {options[2]}
+            </p>
+            <p className="w-96 border rounded-lg px-4 py-4 focus:outline-none focus-visible:ring-violet-400">
+              {options[3]}
+            </p>
           </div>
         </div>
-
-        <div class="bottom-container flex justify-end">
-          <div class="social">
-            <div class="cursor-pointer">
-              <facebook-share
-                url="https://www.w3docs.com/quiz/question/ZGp3"
-                description="www.w3docs.com quiz single question"
-                img="https://www.w3docs.com/uploads/media/quiz_gallery/0001/05/c6cd9c8324951d3fa64593ae9eea6959ade025d7.png"
-                title=" React is mainly used for building ___.
-                                        "
-              />
-            </div>
-          </div>
-          <div class="next_button">
-            <a
-              click="next()"
-              v-show="questionIndex !== 20"
-              class="btn btn-green hover:bg-green-500 cursor-pointer"
-            >
-              Next
-            </a>
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
