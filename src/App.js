5f9ce0440f1d4114d8e3656f3dbc65f8df847740
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-10 20:31:28 +0600

    Header jsx clr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Header></Header>
     </div>
   );
 }
