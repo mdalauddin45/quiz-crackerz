7197422de70fcfac544cc86d9e5ccfdba101c42f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-10 20:38:09 +0600

    ErroPage complet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // import logo from "./logo.svg";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./components/ErrorPage";
 import Main from "./components/Main";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Main></Main> }]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+    },
+  ]);
   return (
     <div>
       <RouterProvider router={router} />
